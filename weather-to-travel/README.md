a0bf8bbad7ac3ca50d0f49ad5e79e8d2b49df403
brynniebear <williamsbrynn@gmail.com>
2021-07-06 14:15:15 -0700

    Remove unnecessary files, add axios, update files

---
+++ weather-to-travel/README.md
@@ -0,0 +1,1 @@
+A weather site for comparing the weather forecast over long distances, created using create-react-app.
---
+++ weather-to-travel/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      Hello from Weather to Travel App!
+    </div>
+  );
+}
